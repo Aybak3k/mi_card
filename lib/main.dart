1c337c0ed2167dd7dd9a6791c115ee5f5aa7ce4a
Aybak3k <nooreldin3000@gmail.com>
2022-04-02 15:38:07 +0200

    add circle avatar && desc text

--- lib/main.dart
+++ lib/main.dart
@@ -13,18 +13,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Center(
-            child: Text(
-              "Aybak3k",
-              style: TextStyle(
-                fontFamily: 'Inconsolata'
+        backgroundColor: Color(0xFF010103),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("images/pfp.jpg"),
               ),
-              )
-            ),
+              SizedBox(height: 10.0,),
+              Text(
+                "Aybak3k",
+                style: TextStyle(
+                  fontFamily: "Inconsolata",
+                  fontSize: 40.0,
+                  color: Color(0xFF7e1e22),
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5
+                )
+              ),
+              SizedBox(height: 10.0,),
+              Text(
+                "CRAZY TECH GEEK",
+                style: TextStyle(
+                  color: Color(0xFF7e1e22),
+                  fontSize: 20.0,
+                  letterSpacing: 5.0
+              ),
+                ),
+                
+
+              
+            ],
+          ),
         ),
-        body: Image.asset('images/pfp.jpg'),
       )
     );
   }
